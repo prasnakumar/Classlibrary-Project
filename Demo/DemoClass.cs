30e5babf1dd141610eeaee68fddd7ddcd86a2308
Prasanna Kumar N <Prasanna.Augmento10@gmail.com>
2022-07-12 22:54:29 +0530

    Add project files.

---
+++ Demo/DemoClass.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Demo
+{
+    public class DemoClass
+    {
+        public void  display()
+        {
+            Console.WriteLine("I am great");
+        }
+    }
+}
---
+++ Print/Program.cs
@@ -0,0 +1,14 @@
+﻿using Demo;
+using System;
+
+namespace Print
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DemoClass message = new DemoClass();
+            message.display();
+        }
+    }
+}
